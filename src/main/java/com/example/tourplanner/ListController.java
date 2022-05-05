7eb14643585466818b3ccff99b6b54bc5e7c953e
LapinEarth <furian.marcel99@gmail.com>
2022-05-05 18:18:30 +0200

    Include listview

--- src/main/java/com/example/tourplanner/ListController.java
+++ src/main/java/com/example/tourplanner/ListController.java
@@ -1,4 +1,23 @@
 package com.example.tourplanner;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ListController {
+    @FXML
+    protected void onAddTourButtonClick() throws IOException {
+        final Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("views/createTour.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
+        stage.setResizable(false);
+        //TODO set Max width to Screen
+        //stage.setMaxWidth(Screen);
+        stage.setTitle("Tour-Planner");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/example/tourplanner/controllers/MainViewController.java
+++ src/main/java/com/example/tourplanner/controllers/MainViewController.java
@@ -10,16 +10,4 @@ import java.io.IOException;
 
 public class MainViewController {
 
-    @FXML
-    protected void onAddTourButtonClick() throws IOException {
-        final Stage stage = new Stage();
-        FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("views/createTour.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
-        stage.setResizable(false);
-        //TODO set Max width to Screen
-        //stage.setMaxWidth(Screen);
-        stage.setTitle("Tour-Planner");
-        stage.setScene(scene);
-        stage.show();
-    }
 }
